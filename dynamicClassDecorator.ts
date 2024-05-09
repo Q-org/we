423e486c3a377f0d9810337fcc05eca925d7d35a
Q-org <raowj@outlook.com>
2024-05-09 12:38:53 +0800

    2

---
+++ dynamicClassDecorator.ts
@@ -0,0 +1,34 @@
+import { replace, upperCase } from "lodash";
+
+(() => {
+  function beforeFunction() {
+    console.log("Running before the main function");
+  }
+
+  function afterFunction() {
+    console.log("Running after the main function");
+  }
+
+  const handler = {
+    apply: function (target, thisArg, argumentsList) {
+      beforeFunction();
+      console.log(`调用了函数: ${target.name}`);
+      // 在这里，thisArg 就是原始函数调用的上下文
+      const result = target.apply(thisArg, argumentsList);
+      afterFunction();
+      return result;
+    },
+  };
+
+  const testObject = {
+    testMethod() {
+      console.log(this); // 这里的 this 将引用 testObject
+      console.log("hello".toUpperCase());
+    },
+  };
+
+  // 使用代理包装 testObject.testMethod
+  const proxiedTestMethod = new Proxy(testObject.testMethod, handler);
+
+  proxiedTestMethod.call(testObject); // 使用 call 方法指定上下文为 testObject
+})();
--- errors.ts
+++ errors.ts
@@ -4,24 +4,20 @@ import Decorator from "@wei/types/src/lib/types/Decorator";
 import { trace } from "console";
 
 (() => {
-  const decorator = new Decorator("toUpperCase");
   // 准备
+  Decorator.callers = ["allowedCaller"];
+  Decorator.doNothing = false;
 
   // 定义一个测试函数
   function testFunction(arg: string) {
     return arg.toUpperCase();
   }
-  //@ts-ignore
 
-  Decorator.callers = [testFunction, "caller2"];
-  Decorator.doNothing = false;
-
-  Decorator.afterCallback = (func: Function, result: unknown, args: any[]) => {
-    console.log(`_____________`, result);
-  };
+  // 应用装饰器
+  const originalMethod = testFunction;
+  // 使用示例
+  const decorator = new Decorator();
+  decorator.apply();
   // 调用原始方法
-  const result = testFunction("hello");
-  console.log(global.queueMicrotask);
-  // 断言
-  // expect(result).toBe("HELLO");
+  const result = originalMethod("hello");
 })();
