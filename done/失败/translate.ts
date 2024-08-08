f9b3223a1d0fe5507ea61fc591f00fb711e52520
Q-org <raowj@outlook.com>
2024-08-08 21:11:53 +0800

    cls

---
+++ done/失败/translate.ts
@@ -0,0 +1,5 @@
+// import translate from "google-translate-api"; 超时
+// import translate from "google-translate-open-api"超时 ;
+// import { GoogleTranslator } from "deep-translator"; python
+
+(async () => {})();
---
+++ test/simple.ts
@@ -0,0 +1,8 @@
+const x: number = 100;
+function greet(name: string, arg1 = 2, arg2 = 3): string {
+  console.log(name);
+  function fn1() {
+    xxx函数(...arguments);
+  }
+  return "Hello, " + name;
+}
